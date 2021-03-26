23d9b3c32d04ec4f0df57a921b35271419104535
Richard Cao <richacao@amazon.com>
2021-03-25 22:21:37 -0700

    Made the UX more friendly for both the developer and the trader

--- hummingbird/module/module.py
+++ hummingbird/module/module.py
@@ -6,13 +6,13 @@ import json
 """
 Base definition of a Module (for now):
 
-1 input, 1 output - specified when pipeline.generate() is called, since that's when
+1 input, 1 output - specified when pipeline.synthesize() is called, since that's when
                     the Dockerfiles (and hence, args for the command) and docker-compose
-                    pipeline file are generated.
+                    pipeline file are synthesized.
 base args           - defined in this file. specified as input to Pipeline and set during 
-                    pipeline.generate()
+                    pipeline.synthesize()
 customs args        - defined when the creator defines their own custom Module. specified
-                    as input to Pipeline and set during pipeline.generate()
+                    as input to Pipeline and set during pipeline.synthesize()
 
 What are the base args?
 type                - input, output, both, or neither. Specified in the definition of the
@@ -35,25 +35,45 @@ For I/O parameters:
 
 Each custom-made module should specify which inputs and outputs are required and/or optional.
 Params are passed as messages in JSON form.
-When a pipeline is generated, it should check if the I/O params are valid matches.
+When a pipeline is synthesized, it should check if the I/O params are valid matches.
 
 """
 class Module(object):
-  def __init__(self, args):
+  def __init__(self):
     # Default base args
     self.args = {
+      # These are defined in the module implementation
+      'has_input'         : False,
+      'has_output'        : False,
+      # These can be set by the user
+      'session_timeout_ms': 10000,
+      'auto_offset_reset' : 'earliest',
+      # These will be generated during pipeline.synthesize()
       'topics_in'         : '',
       'topics_out'        : '',
       'servers_in'        : '',
       'servers_out'       : '',
-      'group.id'          : 'test',
-      'session.timeout.ms': 10000,
-      'auto.offset.reset' : 'earliest'
+      'group_id'          : 'test'
     }
-    
+    self.custom_options = {}
+  
+  def setInput(self, val: bool):
+    self.args['has_input'] = val
+
+  def setOutput(self, val: bool):
+    self.args['has_output'] = val
+
+  def add_argument(self, option, parser=None):
+    self.custom_options[option] = parser
+
+  def build(self, args):
     short_options = ""
-    long_options = ["topics-in=", "topics-out=", "servers-in=", "servers-out=",
-                    "group-id=", "session-timeout-ms=", "auto-offset-reset="]
+    long_options = ["topics-in=", "topics-out=", "servers-in=", 
+                    "servers-out=", "group-id=", "session-timeout-ms=", 
+                    "auto-offset-reset="] + map(
+                      lambda x: (self.custom_options[x] + '=') if self.custom_options[x] != None else self.custom_options[x],
+                      self.custom_options
+                    )
 
     try:
       arguments, _ = getopt.getopt(args, short_options, long_options)
@@ -68,23 +88,30 @@ class Module(object):
         elif currentArgument in ("--servers-out"):
           self.args['servers_out'] = currentValue.split(',')
         elif currentArgument in ("--group-id"):
-          self.args['group.id'] = currentValue
+          self.args['group_id'] = currentValue
         elif currentArgument in ("--session-timeout-ms"):
-          self.args['session.timeout.ms'] = int(currentValue)
+          self.args['session_timeout_ms'] = int(currentValue)
         elif currentArgument in ("--auto-offset-reset"):
-          self.args['auto.offset.reset'] = currentValue
+          self.args['auto_offset_reset'] = currentValue
+        else:
+          # Check through all of the custom options
+          for cop in self.custom_options.keys():
+            if currentArgument in ("--" + cop):
+              if self.custom_options[cop]:
+                self.args[cop] = self.custom_options[cop](currentValue)
+              else:
+                pass # depends
                 
     except getopt.error as err:
       print(str(err))
 
-  def buildIO(self):
     if 'i' in self.args['type']:
       print('Creating consumer')
       conf_in = {
         'bootstrap.servers' : self.args['servers_in'][0],
-        'group.id'          : self.args['group.id'],
-        'session.timeout.ms': self.args['session.timeout.ms'],
-        'auto.offset.reset' : self.args['auto.offset.reset']
+        'group.id'          : self.args['group_id'],
+        'session.timeout.ms': self.args['session_timeout_ms'],
+        'auto.offset.reset' : self.args['auto_offset_reset']
       }
       self.consumer = Consumer(conf_in)
       self.consumer.subscribe(self.args['topics_in'])
