b2d5aebb32f916e35c4ec36f745b2bf42d13b712
Richard Cao <richacao@amazon.com>
2021-03-28 17:27:31 -0700

    Changing version back to 0.0.1 as last-ditch attempt to fix docker issue with pip install hummingbird

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hummingbird',
-    version='0.0.2',
+    version='0.0.1',
     author='Richard Cao',
     author_email='richardycao@gmail.com',
     license='MIT',
